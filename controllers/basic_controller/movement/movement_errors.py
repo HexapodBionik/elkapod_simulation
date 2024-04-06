ca68ddcbcf11982be8965f8007f580673356a78a
Antoni Przybylik <antoni.przybylik@wp.pl>
2024-04-06 13:07:37 +0200

    Removed no-longer-used controlers.

--- controllers/basic_controller/movement/movement_errors.py
+++
@@ -1,9 +0,0 @@
-
-class MovementCoreInitializationError(Exception):
-    def __init__(self, message: str):
-        super().__init__(message)
-
-
-class MovementCoreRuntimeError(Exception):
-    def __init__(self, message: str):
-        super().__init__(message)
